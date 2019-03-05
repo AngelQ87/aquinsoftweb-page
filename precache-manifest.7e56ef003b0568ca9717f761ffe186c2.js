9ee1e0efa9ea272a7047242e7aafc7e2fa80ed48
AngelQ87 <angelquintero87@gmail.com>
2019-03-05 16:49:32 -0500

    para publicar

---
+++ precache-manifest.7e56ef003b0568ca9717f761ffe186c2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6a423536353388f3eae",
+    "url": "/static/css/main.a4e6701e.chunk.css"
+  },
+  {
+    "revision": "b6a423536353388f3eae",
+    "url": "/static/js/main.b6a42353.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e2ee211c1022a1928ba9",
+    "url": "/static/css/2.ff765767.chunk.css"
+  },
+  {
+    "revision": "e2ee211c1022a1928ba9",
+    "url": "/static/js/2.e2ee211c.chunk.js"
+  },
+  {
+    "revision": "cd4dc124b66c1ae7eb3acd2bed8d154e",
+    "url": "/static/media/44.cd4dc124.jpg"
+  },
+  {
+    "revision": "f5bd55735a9132467ec2dfbf7377e424",
+    "url": "/static/media/83.f5bd5573.jpg"
+  },
+  {
+    "revision": "d9eea8304daf9915fd169ee589451fc5",
+    "url": "/static/media/computer.d9eea830.jpg"
+  },
+  {
+    "revision": "a6d1146c5f0bd56b15843c2e0e8f7483",
+    "url": "/index.html"
+  }
+];
